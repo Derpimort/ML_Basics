26493429f0c39567e2f2d606eb6adec96bae76d2
Derpimort <jatinsaini580@gmail.com>
2020-05-28 19:02:10 +0530

    Text stats updated to include sentences preprocessing

--- utils/text_preprocessing/textstats.py
+++ utils/text_preprocessing/textstats.py
@@ -13,6 +13,30 @@ from nltk.corpus import stopwords
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
 from textblob import TextBlob
 from pandas.core.groupby.groupby import DataError
+from nltk.stem import WordNetLemmatizer
+from gensim.utils import simple_preprocess
+from tqdm.auto import tqdm
+
+STOPWORDS = set(stopwords.words('english'))
+lemma = WordNetLemmatizer()
+
+def lemma_pp(text):
+    result = []
+    for token in simple_preprocess(text):
+        if token not in STOPWORDS:
+            result.append(lemma.lemmatize(token))
+    return result
+
+def get_sentences(corpus, progress=False):
+    sentences=[]
+    if progress:
+        corpus=tqdm(corpus)
+    for desc in corpus:
+        try:
+            sentences.append(" ".join(lemma_pp(desc)))
+        except Exception as e:
+            sentences.append(None)
+    return sentences
 
 def check_divide(a,b):
         if b==0:
